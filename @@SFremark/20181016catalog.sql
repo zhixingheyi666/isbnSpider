862cbf0d1172a3399ab4c6480374829baacec67d
zhixingheyi666 <zhixingheyi666@126.com>
2018-10-16 23:07:47 +0800

    1.补充了 辅助编目库中各个表的用途的说明。

--- @@SFremark/20181016catalog.sql
+++ @@SFremark/20181016catalog.sql
@@ -1,10 +1,23 @@
 -- -- truncate table preIsbn;
 -- select * from preisbn;
+-- select count(*) from abkbooklist;
 
 
 -- '验证replace函数的用法'
 -- select replace(isbn,'-','') from attbooklist;
 -- select bl.isbn, bl.title, pr.isbn from attbooklist bl, preisbn pr where pr.isbn = replace(bl.isbn,'-','');
+
+-- '将每次获取的书籍的编目信息并入到辅助编目库中去，同时select into到备份表abklistbook中, 然后记得清空attbooklist，避免重复'
+-- insert into booklist(adds, BCid, Caste, Epitome,  extName, ISBN, keyword, PageMode, Pages, Price, PubID, publish, PublishDate, Title, translator, Version, Writer) 
+-- 	select adds, BCid, Caste, Epitome,  extName, ISBN, keyword, PageMode, Pages, Price, PubID, publish, PublishDate, Title, translator, Version, Writer from attbooklist;
+-- insert into abkbooklist(adds, BCid, Caste, Epitome,  extName, ISBN, keyword, PageMode, Pages, Price, PubID, publish, PublishDate, Title, translator, Version, Writer)
+-- 	select adds, BCid, Caste, Epitome,  extName, ISBN, keyword, PageMode, Pages, Price, PubID, publish, PublishDate, Title, translator, Version, Writer from attbooklist;
+
+-- -- truncate attbooklist;
+
+
+-- '查询一个表所有的列名'
+-- select name from syscolumns where id=(select max(id) from sysobjects where xtype='u' and name='attbooklist')
 
 -- select * from booklist;
 -- select * from attbooklist;
